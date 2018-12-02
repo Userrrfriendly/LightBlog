e55c75c3f27232a7395ebcd7d47246f32c6bfd36
Veniamin Tsigourof <tsigourof_ben@hotmail.com>
2018-12-02 12:27:57 +0200

    sync: sync

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,48 @@
 import React from 'react';
+import { Provider } from '../Context';
 import { Switch, Route } from 'react-router-dom';
-
 import Home from '../../components/Home/Home';
 
 
 class App extends React.Component {
   state = {
-    articles:[]
+    articles:[
+      // {title:'Title TEst',
+      //   author:'Author Test',
+      //   body:'body TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST',
+      //   createdAt:"Fri Nov 30 2017 22:24:22 GMT+0200 (Eastern European Standard Time)",
+      //   _id:"12345678901"
+      // }
+    ]
+  }
+
+  onLoad = (articles)=> {
+    
+    // const test = ()=>{console.log(this)}
+    // test();
+    console.log(articles)
+    // const that = this;
+    // console.log(that);
+    this.setState({
+      articles:articles
+    });
+    // console.log('...fetching all articles from server');
   }
 
   render () {
     return (
-      <Switch>
-        <Route exact path="/" render={ ()=> {
-          return (<Home articles={this.state.articles}/>)}
-        } />
-        <Route path="/about" render={()=>{return (<h1>ABOUT</h1>)}} />
-        <Route path="/about/me" component={Home} />
-      </Switch>
+      <Provider value={{
+        state: this.state,
+        actions: {
+          onLoad: this.onLoad
+        }
+      }}>
+        <Switch>
+          <Route exact path="/" render={()=> <Home />} />
+          <Route exact path="/about" render={()=>{return (<h1>ABOUT</h1>)}} /> 
+          <Route path="/about/me" render={()=>{return (<h2>ABOUT MEEE!!!!!!!!!!</h2>)}} />
+        </Switch>
+      </Provider>
     )
   }
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 import axios from 'axios';
 import moment from 'moment'; //JavaScript date library for parsing, validating, manipulating, and formatting dates.
 
+import { Consumer, AppContext } from '../Context';
 import { Form } from '../../components/Article';
 
 class Home extends React.Component {
@@ -14,11 +15,38 @@ class Home extends React.Component {
   }
 
   componentDidMount() {
-    // console.log(this.props);
-    const { onLoad } = this.props;
-    console.log(onLoad)
-    axios('http://localhost:8000/api/articles')
-      .then((res) => onLoad(res.data));
+    // let value = this.context;
+    // console.log(this.props.onLoad);
+
+    // this.props.onLoad(
+    //   [
+    //     {title:'AATitle TEst',
+    //       author:'AAAAuthor Test',
+    //       body:'AAAbody TEST',
+    //       createdAt:"Fri Nov 20 2017 22:24:22 GMT+0200 (Eastern European Standard Time)",
+    //       _id:"A12345678901"
+    //     }
+    //   ]
+    // );
+
+    this.context.actions.onLoad(
+      [
+        {title:'AATitle TEst',
+          author:'AAAAuthor Test',
+          body:'AAAbody TEST',
+          createdAt:"Fri Nov 20 2017 22:24:22 GMT+0200 (Eastern European Standard Time)",
+          _id:"A12345678901"
+        }
+      ]
+    );
+
+    // console.log(value.actions.onLoad);
+    // value.actions.onLoad();
+    // const { onLoad } = this.props;
+    // const onLoadz = value.actions.getArticles;
+    // axios('http://localhost:8000/api/articles')
+    //   .then((res) => onLoad(res.data))
+    //   .catch(onLoadz);
   }
 
   handleDelete(id) {
@@ -35,49 +63,57 @@ class Home extends React.Component {
   }
 
   render() {
-    // console.log(this.props)
-    const { articles } = this.props;
+    // const { articles } = this.props;
 
     return (
-      <div className="container">
-        <div className="row pt-5">
-          <div className="col-12 col-lg-6 offset-lg-3">
-            <h1 className="text-center">LightBlog</h1>
-          </div>
-          <Form />
-        </div>
-        <div className="row pt-5">
-          <div className="col-12 col-lg-6 offset-lg-3">
-            {articles.map((article) => {
-              return (
-                <div className="card my-3">
-                  <div className="card-header">
-                    {article.title}
-                  </div>
-                  <div className="card-body">
-                    {article.body}
-                    <p className="mt-5 text-muted"><b>{article.author}</b> {moment(new Date(article.createdAt)).fromNow()}</p>
-                  </div>
-                  <div className="card-footer">
-                    <div className="row">
-                      <button onClick={() => this.handleEdit(article)} className="btn btn-primary mx-3">
-                        Edit
-                      </button>
-                      <button onClick={() => this.handleDelete(article._id)} className="btn btn-danger">
-                        Delete
-                      </button>
+      <Consumer>
+        {context => {
+          // const context = Consumer;
+          console.log(context);
+          return (
+            <div className="container">
+            <div className="row pt-5">
+              <div className="col-12 col-lg-6 offset-lg-3">
+                <h1 className="text-center">LightBlog</h1>
+              </div>
+              <Form />
+            </div>
+            <div className="row pt-5">
+              <div className="col-12 col-lg-6 offset-lg-3">
+                {context.state.articles.map((article) => {
+                  return (
+                    <div key={article._id}className="card my-3">
+                      <div className="card-header">
+                        {article.title}
+                      </div>
+                      <div className="card-body">
+                        {article.body}
+                        <p className="mt-5 text-muted"><b>{article.author}</b> {moment(new Date(article.createdAt)).fromNow()}</p>
+                      </div>
+                      <div className="card-footer">
+                        <div className="row">
+                          <button onClick={() => this.handleEdit(article)} className="btn btn-primary mx-3">
+                            Edit
+                          </button>
+                          <button onClick={() => this.handleDelete(article._id)} className="btn btn-danger">
+                            Delete
+                          </button>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              )
-            })}
+                  )
+                })}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+          )
+        }} 
+      </Consumer>
+
     );
   }
 }
-
+Home.contextType = AppContext;
 // const mapStateToProps = state => ( {
 //   articles: state.home.articles,
 // });
